399501eba1452894eeaa836a2791ba078029e79c
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-01-27 21:17:39 +0100

    format main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "rg-AVD-int-dewc-1"
   location = "westeurope"
-  
+
   tags = {
     Location = "germanywestcentral"
     Owner    = "Olad, Koosha"
